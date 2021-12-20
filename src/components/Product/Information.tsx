b74e724261aa1f1418e38fb748d42145276a4579
Sherly Febrianti <sherlyfebrianti96@yahoo.co.id>
2021-12-20 02:20:44 +0100

    Update styling in Product Detail Page

--- src/components/Product/Information.tsx
+++ src/components/Product/Information.tsx
@@ -1,13 +1,17 @@
+import { AddShoppingCart } from "@mui/icons-material";
 import {
+  Button,
   FormControl,
   Grid,
   InputLabel,
   MenuItem,
   Select,
   SelectChangeEvent,
+  Theme,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { OptionsColor } from "../../enum/OptionsColor";
@@ -21,8 +25,17 @@ export interface ProductInformationProps {
   color?: string;
 }
 
+const useSectionStyles = makeStyles((theme: Theme) => ({
+  root: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+  },
+}));
+
 export const ProductInformation: React.FunctionComponent<ProductInformationProps> =
   ({ item, ...props }) => {
+		const sectionStyles = useSectionStyles();
+
     const [activeOption, setActiveOption] = useState<ProductOption>(
       item.options[0]
     );
@@ -95,7 +108,7 @@ export const ProductInformation: React.FunctionComponent<ProductInformationProps
           <small>Provided by</small> {item.brand.toUpperCase()}
         </Typography>
 
-        <Grid container sx={{ marginTop: 3, marginBottom: 3 }}>
+        <Grid container classes={sectionStyles}>
           <Grid item>
             <Typography variant="h5">
               NOK <strong>{priceFormat(item.price)}</strong>,-
@@ -110,7 +123,7 @@ export const ProductInformation: React.FunctionComponent<ProductInformationProps
           container
           direction="column"
           spacing={1}
-          sx={{ marginTop: 3, marginBottom: 3 }}
+          classes={sectionStyles}
         >
           <Grid item>
             <Grid container spacing={2}>
@@ -155,26 +168,40 @@ export const ProductInformation: React.FunctionComponent<ProductInformationProps
             </Grid>
           </Grid>
         </Grid>
-        <Grid item>
-          <FormControl>
-            <InputLabel id="select-color-label">Color</InputLabel>
-            <Select
-              labelId="select-color-label"
-              id="select-color"
-              value={activeOption.color.toString()}
-              label="Color"
-              onChange={onColorChange}
+
+        <Grid container classes={sectionStyles}>
+          <Grid item>
+            <FormControl>
+              <InputLabel id="select-color-label">Color</InputLabel>
+              <Select
+                labelId="select-color-label"
+                id="select-color"
+                value={activeOption.color.toString()}
+                label="Color"
+                onChange={onColorChange}
+              >
+                {item.options.map((option) => (
+                  <MenuItem
+                    key={`color-selection-${option.color.toString()}`}
+                    value={option.color.toString()}
+                  >
+                    {option.color.toString().toUpperCase()}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container classes={sectionStyles}>
+          <Grid item>
+            <Button
+              variant="contained"
+              size="large"
+              startIcon={<AddShoppingCart />}
             >
-              {item.options.map((option) => (
-                <MenuItem
-                  key={`color-selection-${option.color.toString()}`}
-                  value={option.color.toString()}
-                >
-                  {option.color.toString().toUpperCase()}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+              Add to cart
+            </Button>
+          </Grid>
         </Grid>
       </>
     );
